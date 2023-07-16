ab1ff24e9d11f97fbf54ba43e192614fea48ade6
yusifizcode <5ba0gh2@code.edu.az>
2023-07-16 14:21:23 +0400

    feat: businnes layer added into Catalog.API

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories;
+
+public interface IProductRepository
+{
+    Task<Product> GetProductByIdAsync(string id);
+    Task<IEnumerable<Product>> GetProductsAsync();
+    Task<IEnumerable<Product>> GetProductByNameAsync(string name);
+    Task<IEnumerable<Product>> GetProductByCategoryAsync(string categoryName);
+
+    Task CreateProductAsync(Product product);
+    Task<bool> DeleteProductAsync(string id);
+    Task<bool> UpdateProductAsync(Product product);
+}
