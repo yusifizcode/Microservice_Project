8f45becb27b371845128f69503f8451975681c51
yusifizcode <5ba0gh2@code.edu.az>
2023-07-18 00:15:19 +0400

    feat: Discount.Grpc microservice completed

---
+++ src/Services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Grpc.Entities;
+
+namespace Discount.Grpc.Repositories;
+
+public interface IDiscountRepository
+{
+    Task<Coupon> GetDiscountAsync(string productName);
+
+    Task<bool> CreateDiscountAsync(Coupon coupon);
+    Task<bool> UpdateDiscountAsync(Coupon coupon);
+    Task<bool> DeleteDiscountAsync(string productName);
+}
