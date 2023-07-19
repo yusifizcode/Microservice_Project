656091156322d6da1a838dafe4c25e8d502644a4
yusifizcode <5ba0gh2@code.edu.az>
2023-07-19 14:46:48 +0400

    feat: developing Ordering.Application Layer - Application features

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList;
+
+public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+{
+    private readonly IMapper _mapper;
+    private readonly IOrderRepository _orderRepository;
+
+    public GetOrdersListQueryHandler(IMapper mapper,
+                                     IOrderRepository orderRepository)
+    {
+        _mapper = mapper;
+        _orderRepository = orderRepository;
+    }
+
+    public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+    {
+        var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+        return _mapper.Map<List<OrdersVm>>(orderList);
+    }
+}
