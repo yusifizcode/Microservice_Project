a5c2d856024de2fb7819ae7dbaff39a58b30a18c
yusifizcode <5ba0gh2@code.edu.az>
2023-07-20 14:03:12 +0400

    feat: application command features - checkout

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+
+public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand, int>
+{
+    private readonly IMapper _mapper;
+    private readonly IEmailService _emailService;
+    private readonly IOrderRepository _orderRepository;
+    private readonly ILogger<CheckoutOrderCommandHandler> _logger;
+
+    public CheckoutOrderCommandHandler(IMapper mapper,
+                                       IEmailService emailService,
+                                       IOrderRepository orderRepository,
+                                       ILogger<CheckoutOrderCommandHandler> logger)
+    {
+        _mapper = mapper;
+        _logger = logger;
+        _emailService = emailService;
+        _orderRepository = orderRepository;
+    }
+
+    public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+    {
+        var orderEntity = _mapper.Map<Order>(request);
+        var newOrder = await _orderRepository.AddAsync(orderEntity);
+
+        _logger.LogInformation($"Order {newOrder.Id} is successfully created.");
+
+        await SendMail(newOrder);
+
+        return newOrder.Id;
+    }
+
+    private async Task SendMail(Order order)
+    {
+        var email = new Email() { To = "yusifosmanov475@gmail.com", Body = $"Order was created.", Subject = "Order was created" };
+
+        try
+        {
+            await _emailService.SendMail(email);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Order {order.Id} failed due to an error with the mail service: {ex.Message}");
+        }
+    }
+}
