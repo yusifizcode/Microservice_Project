3ac736bd763fed12e9319b534230e063f08d16ed
yusifizcode <5ba0gh2@code.edu.az>
2023-07-21 18:22:22 +0400

    feat: developing Ordering.Infrastructure - repositories

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.Infrastructure.Repositories;
+
+public class OrderRepository : BaseRepository<Order>, IOrderRepository
+{
+    public OrderRepository(OrderContext context) : base(context) { }
+
+    public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+    {
+        var orderList = await _context.Orders
+                                .Where(o => o.UserName == userName)
+                                .ToListAsync();
+
+        return orderList;
+    }
+}
