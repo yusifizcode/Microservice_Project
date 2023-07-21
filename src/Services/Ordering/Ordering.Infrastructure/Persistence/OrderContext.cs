7ef23543cb501346ec11b57f2396e0f8f506d82f
yusifizcode <5ba0gh2@code.edu.az>
2023-07-21 16:56:15 +0400

    feat: developing Ordering.Infrastructure.Persistence

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence;
+
+public class OrderContext : DbContext
+{
+    public OrderContext(DbContextOptions<OrderContext> options) : base(options) { }
+
+    public DbSet<Order> Orders { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedDate = DateTime.Now;
+                    entry.Entity.CreatedBy = "yusifiz";
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedDate = DateTime.Now;
+                    entry.Entity.LastModifiedBy = "yusifiz";
+                    break;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence;
+
+public class OrderContextSeed
+{
+    public static async Task SeedAsync(OrderContext context, ILogger<OrderContextSeed> logger)
+    {
+        if (!context.Orders.Any())
+        {
+            context.Orders.AddRange(GetPreconfiguredOrders());
+            await context.SaveChangesAsync();
+            logger.LogInformation($"Seed database associated with context {typeof(OrderContext).Name}");
+        }
+    }
+
+    private static IEnumerable<Order> GetPreconfiguredOrders()
+    {
+        return new List<Order>
+            {
+                new Order() {UserName = "yusifiz", FirstName = "Yusif", LastName = "Osmanov", EmailAddress = "osmanov.yusif@gmail.com", AddressLine = "Sumgait", Country = "Azerbaijan", TotalPrice = 1350 }
+            };
+    }
+}
