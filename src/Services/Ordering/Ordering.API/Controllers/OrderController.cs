35ff6253c2c776e51cfbde7fd744759dd53be353
yusifizcode <5ba0gh2@code.edu.az>
2023-07-21 16:36:07 +0400

    feat: developing Ordering.API presentation layer

---
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController : ControllerBase
+{
+    private readonly IMediator _mediator;
+}
