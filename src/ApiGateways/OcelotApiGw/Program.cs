0ddabb22f5585da1369bac91f8ae80f91a3fc8ec
yusifizcode <5ba0gh2@code.edu.az>
2023-07-27 18:38:18 +0400

    feat: response caching in OcelotApiGw with configuring Ocelot.json file

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,10 +1,11 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
-builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddOcelot(builder.Configuration).AddCacheManager(settings => settings.WithDictionaryHandle());
 
 var app = builder.Build();
 
