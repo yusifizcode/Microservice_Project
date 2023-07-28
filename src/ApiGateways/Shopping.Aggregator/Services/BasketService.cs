97f51bfa3ccecd83f2ea51350732a7ebdb878705
yusifizcode <5ba0gh2@code.edu.az>
2023-07-28 13:44:04 +0400

    feat: developing service classes for consuming internal microservices in Shopping.Aggr

---
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,16 @@
+﻿using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services;
+
+public class BasketService : IBasketService
+{
+    private readonly HttpClient _client;
+
+    public BasketService(HttpClient client)
+        => _client = client;
+
+    public Task<BasketModel> GetBasket(string userName)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,16 @@
+﻿using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly HttpClient _client;
+
+    public OrderService(HttpClient client)
+        => _client = client;
+
+    public Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+    {
+        throw new NotImplementedException();
+    }
+}
