05b4162d794d9b006139d8254784014a4fde70c8
yusifizcode <5ba0gh2@code.edu.az>
2023-07-30 02:18:56 +0400

    feat: SignUp endpoint added into IdentityServer

---
+++ src/IdentityServer/IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer.DTOs;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+            => _userManager = userManager;
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDto.UserName,
+                Email = signUpDto.Email,
+                City = signUpDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signUpDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+    }
+}
