3958725b395f4c86a5ab2ccf77a001a75dcdc5a5
yusifizcode <5ba0gh2@code.edu.az>
2023-07-31 02:13:52 +0400

    fix: IdentityServerConstants.LocalApi.ScopeName added into config file

--- src/IdentityServer/IdentityServer/Controllers/UserController.cs
+++ src/IdentityServer/IdentityServer/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using IdentityServer.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using System.Linq;
 using System.Threading.Tasks;
 using static IdentityServer4.IdentityServerConstants;
 
@@ -36,6 +38,24 @@ namespace IdentityServer.Controllers
             }
 
             return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+            if (userIdClaim == null) return BadRequest();
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+            if (user == null) return BadRequest();
+
+            return Ok(new
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Email = user.Email,
+                City = user.City
+            });
         }
     }
 }
