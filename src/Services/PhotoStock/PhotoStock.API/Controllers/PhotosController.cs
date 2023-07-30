7f07af4cd03c0d19af2070f45a2c97ef30e32902
yusifizcode <5ba0gh2@code.edu.az>
2023-07-31 02:58:01 +0400

    feat: PhotoSave & PhotoDelete endpoints added into PhotosController

---
+++ src/Services/PhotoStock/PhotoStock.API/Controllers/PhotosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoStock.API.DTOs;
+
+namespace PhotoStock.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PhotosController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+    {
+        if (photo != null && photo.Length > 0)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+            using var stream = new FileStream(path, FileMode.Create);
+            await photo.CopyToAsync(stream, cancellationToken);
+
+            var returnPath = "photos/" + photo.FileName;
+
+            PhotoDto photoDto = new() { Url = returnPath };
+
+            return Ok(photoDto);
+        }
+
+        return BadRequest("Photo is empty!");
+    }
+
+    public IActionResult PhotoDelete(string photoUrl)
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+        if (!System.IO.File.Exists(path)) return NotFound("Path not found!");
+
+        System.IO.File.Delete(path);
+        return NoContent();
+    }
+}
